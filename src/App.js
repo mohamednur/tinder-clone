62e4918a7703f39a707fe622faa8d66e1a251306
mohamednur <mohamednurhassan8@gmail.com>
2020-08-06 22:07:02 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import TinderCards from "./TinderCards";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/chat"></Route>
+          <Route path="/">
+            {/* tinder cards*/}
+            <TinderCards></TinderCards>
+          </Route>
+        </Switch>
+
+        {/*Buttons below tinder */}
+
+        {/* chat screens */}
+        {/* individual chat screen*/}
+      </Router>
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TinderCards() {
+  return (
+    <div>
+      <h1>Tinder cards</h1>
+    </div>
+  );
+}
+
+export default TinderCards;
